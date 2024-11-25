022d6cbca7d2a57fd663dcc091b32abc54be317c
giobauermeister <giovanni.bauermeister@gmail.com>
2024-11-25 14:31:12 -0300

    generate resources header with cmakelists and compile into binary. print content and bytes in main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include "index_html.h"
+#include "favicon_ico.h"
 
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!\n");
+
+   printf("Index.html (%d bytes):\n%.*s\n", index_html_len, index_html_len, index_html);
+   printf("Favicon.ico size: %d bytes\n", favicon_ico_len);
+
    return 0;
 }
